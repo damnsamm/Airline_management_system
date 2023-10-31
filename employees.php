5453e415cf13f61480f13637a1ad406b7d5cfb1c
Samika Gandhi <130069182+damnsamm@users.noreply.github.com>
2023-10-31 17:49:33 +0530

    added salary column to emplyees

--- employees.php
+++ employees.php
@@ -81,6 +81,7 @@
             echo "<th>Contact Number</th>";
             echo "<th>Email Address</th>";
             echo "<th>Employee Type</th>";
+            echo "<th>Employee Salary</th>";
             echo "<th>Edit</th>";
             echo "<th>Delete</th>";
             echo "</tr>";
@@ -94,6 +95,7 @@
                     echo "<td>{$row['contact_no']}</td>";
                     echo "<td>{$row['email']}</td>";
                     echo "<td>{$row['type']}</td>";
+                    echo "<td>{$row['salary']}</td>";
                 
         echo "<td><a href='edit_em.php?e_id={$row['e_id']}' class='edit-button'>Edit</a></td>";
                   
